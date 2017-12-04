49c7847e1208a963f0886b69f65d8d7d3262f157
Jonny Ward <JonnyWard@macs-mbp.home>
2017-12-04 10:30:47 +0000

    axios link removed

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import axios from 'axios';
+
 
 import ChallengeRequestSent from './ChallengeRequestSent';
 import GuideStep3 from './GuideStep3';
@@ -16,26 +16,23 @@ class ProfilePage extends Component {
       requestSent: false,
       requestIcon: 'none'
     }
-    this.TweetChamp = this.TweetChamp.bind(this);
     this.SendRequest = this.SendRequest.bind(this);
   }
 
-  TweetChamp(event) {
-    axios.get()
-  }
-
-  SendRequest(){
-    this.setState({ requestSent : true});
+  SendRequest() {
+    console.log(true)
+    this.setState({requestSent: true});
     setTimeout(() => {
-      this.setState({requestIcon : 'block'});
+      console.log(true)
+      this.setState({requestIcon: 'block'});
     }, 3000);
   }
 
   render() {
 
     const toggleButton = () => {
-      if(this.state.requestSent){
-        return <ChallengeRequestSent />;
+      if (this.state.requestSent) {
+        return <ChallengeRequestSent/>;
       }
     }
     // 1) Triggers when to display the notification
@@ -45,33 +42,27 @@ class ProfilePage extends Component {
     };
 
     const doShow = {
-      display: this.state.requestIcon
+      display: 'this.state.requestIcon'
     }
 
     // There must be someway to set Time on the Display variable
-    const display = this.state.requestSent? this.notificationDelay : { display : 'none'}
+    const display = this.state.requestSent
+      ? doShow
+      : dontShow
 
-    const notificationDelay = () => {
-      setTimeout(function() {
-        console.log(this.state.requestSent);
-        return {display : 'block'};
-      }.bind(this), 3000);
-    }
+    return (<div className="main-container">
 
-    return (
-      <div className="main-container">
-
-      {/*Challenge Accepted Banner*/}
-      {/*There still needs to be a timeout period for it appear and disappear*/}
+      {/* Challenge Accepted Banner */}
+      {/* There still needs to be a timeout period for it appear and disappear */}
       <article className="challenge-accepted-banner uk-animation-slide-top" style={display}>
         <div className="message-banner">
-            <img src="Trophy_Icon_White.png" alt="trophy icon"/>
-            <h5 className="challenge-msg">Challenge Accepted</h5> <h6 className="gamer-name">Shaun Gibson</h6>
+          <img src="Trophy_Icon_White.png" alt="trophy icon"/>
+          <h5 className="challenge-msg">Challenge Accepted</h5>
+          <h6 className="gamer-name">Shaun Gibson</h6>
         </div>
       </article>
 
-      <section className="menu-drop-down">
-      </section>
+      <section className="menu-drop-down"></section>
       <section className="activity-name-wrapper">
         <div className="white-gradient"></div>
         <h3>Profile</h3>
@@ -109,14 +100,18 @@ class ProfilePage extends Component {
 
               <div className="challenge-btn-container uk-animation-shake">
 
-              {/*1) Button Clicked*/}
-              <button className="challenge-btn" onClick={this.SendRequest} style={{display: this.state.requestSent? 'none' : 'flex'}}>
-                <img src="Provoke_Icon_Brown.png" alt="challenge icon"/>
-                <h4>Challenge</h4>
-              </button>
+                {/* 1) Button Clicked */}
+                <button className="challenge-btn" onClick={this.SendRequest} style={{
+                    display: this.state.requestSent
+                      ? 'none'
+                      : 'flex'
+                  }}>
+                  <img src="Provoke_Icon_Brown.png" alt="challenge icon"/>
+                  <h4>Challenge</h4>
+                </button>
 
-              {/*2) Toggles to this button*/}
-              {toggleButton()}
+                {/* 2) Toggles to this button */}
+                {toggleButton()}
 
               </div>
 
@@ -124,7 +119,7 @@ class ProfilePage extends Component {
           </div>
         </article>
       </section>
-      {/*Navigation Bar*/}
+      {/* Navigation Bar */}
       <section className="nav-bar">
         <div className="horizontal-line"></div>
         <article className="nav-btn-container">
@@ -149,20 +144,19 @@ class ProfilePage extends Component {
           <div className="ghost-separator"></div>
         </article>
       </section>
-      {/*Display Step 1 Tip*/}
-      {this.state.requestSent
-        ?
-        <GuideStep4/>
-        :
-        <GuideStep3/>
+      {/* Display Step 1 Tip */}
+      {
+        this.state.requestSent
+          ? <GuideStep4/>
+          : <GuideStep3/>
       }
-      {/*Notification Circle*/}
+      {/* Notification Circle */}
       <section className="challenge-notification uk-animation-scale-up" style={display}>
         <div className="notification-amount">
           <h6>1</h6>
         </div>
       </section>
-      {/*Navigation*/}
+      {/* Navigation */}
       <section className="nav-display">
         <article className="logo-wrapper">
           <img className="white-logo" src="BetGame_Logo_White.png" alt="white champ logo"/>
